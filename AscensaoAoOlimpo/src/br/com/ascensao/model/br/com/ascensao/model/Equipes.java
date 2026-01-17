b6d3d6968a52680f1ad3c2db71dafbf9079ba2f7
Maciele Ramos <macieleramostx@gmail.com>
2026-01-17 20:39:40 -0300

    feat - criei novos métodos p/ gerenciar o ataque

--- AscensaoAoOlimpo/src/br/com/ascensao/model/br/com/ascensao/model/Equipes.java
+++ AscensaoAoOlimpo/src/br/com/ascensao/model/br/com/ascensao/model/Equipes.java
@@ -12,7 +12,7 @@ public class Equipes {
         this.ladoA = new ArrayList<SemiDeus>();
         this.ladoB = new ArrayList<SemiDeus>();
     }
-
+    
     private SemiDeus sortearCombatente(String nome) {
         // definimos 4 tipos de combatentes, por isso são 4 faces p/ sortear
         int sorteado = Dado.rolar(4);
@@ -28,7 +28,7 @@ public class Equipes {
 
     }
     // aqui preenchemos as listas p/ cada lado com os combatentes
-    public void iniciarCombate(){
+    public void formarEquipes(){
         // uma batalha pode ser um duelo de 1 contra 1, ou uma guerra massiva de 100 contra 100
         int tam = Dado.rolar(100);
         System.out.println(">>>>>>>>> TORNEIO COMEÇA!!");
@@ -38,8 +38,61 @@ public class Equipes {
             this.ladoA.add(c1);
         
             // escolhe combatente aleatório e adiciona no ladoB
-            SemiDeus c2 = sortearCombatente(">>> Combatente B" + i);
+            SemiDeus c2 = sortearCombatente(">>> Combatente B " + i);
             this.ladoB.add(c2);
+        }
+    }
+
+    public boolean temSobreviventes(ArrayList<SemiDeus> lado){
+        for(SemiDeus guerreiro : lado){
+            if(guerreiro.estaVivo()){
+                return true; // tem combatentes
+            }
+        }
+        return false; // sobrou ninguém
+    }
+
+    /*
+    Quando chega a vez de um combatente agir, ele escolhe um alvo aleato ́rio entre os membros vivos
+    da equipe adversa ́ria e desfere seu ataque caracter ́ıstico. 
+    */
+    public void turnoCombate(ArrayList<SemiDeus> atacantes, ArrayList<SemiDeus> defensores){
+        if(defensores.isEmpty()){ // verificação para ver a lista de defesa está vazia
+                return;
+            }
+            
+        for(SemiDeus a : atacantes){
+            if(!a.estaVivo()){ // para que mortos n ataquem
+                continue;
+            }
+            
+            int i = Dado.rolar(defensores.size()) - 1;
+            SemiDeus alvo = defensores.get(i); // atribui um alvo com base em um indice aleatório da lista de defensores
+            System.out.println(">>> " + a.getNome() + " ataca " + alvo.getNome());
+            a.atacar(alvo); // atacante faz o ataque contra o alvo escolhido
+            if(!alvo.estaVivo()){ // verifica se o alvo morreu para poder remove-lo da lista
+                defensores.remove(alvo);
+            }
+        }
+    }
+
+    public void iniciarCombate(){
+        int quantRodadas = 1;
+        while(temSobreviventes(ladoA) && temSobreviventes(ladoB)){
+            System.out.println("<<<<<< RODADA " + quantRodadas);
+            // lado A ataca
+            turnoCombate(ladoA, ladoB);
+            if(!temSobreviventes(ladoB)){
+                break;
+            }
+            // lado B contra-ataca
+            turnoCombate(ladoB, ladoA);
+            quantRodadas++;
+        }
+        if(temSobreviventes(ladoA)){
+            System.out.println("<<<<< VENCEDOR: LADO A");
+        }else{
+            System.out.println("<<<<< VENCEDOR LADO B!!");
         }
     }
 
