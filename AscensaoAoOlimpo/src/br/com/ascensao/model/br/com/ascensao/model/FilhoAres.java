3144b79998252d391caca778e323ea6394403a8d
Luiza Accioly <luiza.accioly2004@gmail.com>
2026-01-18 14:32:27 -0300

    doc: formata o codigo
    formata os arquivos de código.

--- AscensaoAoOlimpo/src/br/com/ascensao/model/br/com/ascensao/model/FilhoAres.java
+++ AscensaoAoOlimpo/src/br/com/ascensao/model/br/com/ascensao/model/FilhoAres.java
@@ -4,7 +4,7 @@ public class FilhoAres extends SemiDeus {
 
     private double taxaRouboVida;
 
-    public FilhoAres(){
+    public FilhoAres() {
 
     }
 
@@ -15,35 +15,30 @@ public class FilhoAres extends SemiDeus {
 
     @Override
     public void atacar(SemiDeus alvo) {
-    double danoInicial = this.getAtaqueBase()*this.getModificadorDano();//dano no inimigo a priori
-    double vidaInimigoInicial = alvo.getPontosvida(); //vida do inimigo antes do ataque
-    alvo.receberDano(danoInicial);
+        double danoInicial = this.getAtaqueBase() * this.getModificadorDano();// dano no inimigo a priori
+        double vidaInimigoInicial = alvo.getPontosvida(); // vida do inimigo antes do ataque
+        alvo.receberDano(danoInicial);
 
-    double vidaInimigoApos = alvo.getPontosvida(); // vida do inimigo depois do ataque
+        double vidaInimigoApos = alvo.getPontosvida(); // vida do inimigo depois do ataque
 
-    double danoCalculado = vidaInimigoInicial - vidaInimigoApos;//a diferença entre as duas vidas é o dano real.
+        double danoCalculado = vidaInimigoInicial - vidaInimigoApos;// a diferença entre as duas vidas é o dano real.
 
-    if(danoCalculado>0){
-    double valorCura = danoCalculado*taxaRouboVida;//habilidade única, quando causa dano se cura.
-    
-    this.curar(valorCura);
-    }else{
-        this.curar(0.0);
+        if (danoCalculado > 0) {
+            double valorCura = danoCalculado * taxaRouboVida;// habilidade única, quando causa dano se cura.
+
+            this.curar(valorCura);
+        } else {
+            this.curar(0.0);
+        }
+
     }
-    
-    }
-
 
     public double getTaxaRouboVida() {
         return taxaRouboVida;
     }
 
-
-
     public void setTaxaRouboVida(double taxaRouboVida) {
         this.taxaRouboVida = taxaRouboVida;
     }
-    
-    
 
 }
--- AscensaoAoOlimpo/src/br/com/ascensao/util/SorteioBuff.java
+++ AscensaoAoOlimpo/src/br/com/ascensao/util/SorteioBuff.java
@@ -4,29 +4,30 @@ import br.com.ascensao.model.*;
 
 public class SorteioBuff {
 
-     public void aplicarBuffAleatorio (SemiDeus alvo){
+    public void aplicarBuffAleatorio(SemiDeus alvo) {
         int sorteio = Dado.rolar(6);
-        switch (sorteio){
-            case 1://buff dionisio
-                alvo.setEstaAtordoado(true);//atacante nn causa dano pois nn acerta o alvo.
+        switch (sorteio) {
+            case 1:// buff dionisio
+                alvo.setEstaAtordoado(true);// atacante nn causa dano pois nn acerta o alvo.
                 break;
-            case 2://buff hermes
-                alvo.setModificadorDano(1.2); //o dano aumenta em 20%
+            case 2:// buff hermes
+                alvo.setModificadorDano(1.2); // o dano aumenta em 20%
                 break;
-            case 3://buff afrodite
-                alvo.setModificadorDano(0.5);//dano reduz em 50%
+            case 3:// buff afrodite
+                alvo.setModificadorDano(0.5);// dano reduz em 50%
                 break;
-            case 4://buff athena
-                alvo.setModificadorDefesa(1.5);//defesa aumenta em 50%
+            case 4:// buff athena
+                alvo.setModificadorDefesa(1.5);// defesa aumenta em 50%
                 break;
-            case 5://buff poseidon
-                alvo.setTemReflexo(true);//tem escudo de poseidon e dano é reduzido pela metade.(e metade volta ao oponente)
+            case 5:// buff poseidon
+                alvo.setTemReflexo(true);// tem escudo de poseidon e dano é reduzido pela metade.(e metade volta ao
+                                         // oponente)
                 break;
             case 6:// buff deméter
-                alvo.setFrutaSagrada(true);//aumenta vida em 50% estantaneamente
+                alvo.setFrutaSagrada(true);// aumenta vida em 50% estantaneamente
                 break;
         }
-    
+
     }
 
 }
